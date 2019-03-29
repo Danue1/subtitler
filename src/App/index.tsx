e923d42ac4e1722b069cb13d7614fad44df3b597
OnLiU <public.danuel@gmail.com>
2019-03-29 23:21:07 +0900

    add draft layout

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,3 +1,38 @@
 import React, { FC } from 'react'
+import styled from 'styled-components'
+import { Div } from '../Atomics/Div'
 
-export const App: FC = () => <div>App</div>
+const Layout = Div
+
+const Header = styled.header``
+
+const Title = Div
+
+const Main = styled.main``
+
+const Left = Div
+
+const SubtitleList = Div
+
+const Right = Div
+
+const Video = Div
+
+const Timeline = Div
+
+export const App: FC = () => (
+  <Layout>
+    <Header>
+      <Title>Subtitler</Title>
+    </Header>
+    <Main>
+      <Left>
+        <SubtitleList>SubtitleList</SubtitleList>
+      </Left>
+      <Right>
+        <Video>Video</Video>
+        <Timeline>Timeline</Timeline>
+      </Right>
+    </Main>
+  </Layout>
+)
